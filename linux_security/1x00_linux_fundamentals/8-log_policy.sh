da5b6ab57d4b1f74006eaacc2be97e2eb2961012
Cecile.LAROCHE <11458@holbertonstudents.com>
2026-04-09 21:55:34 +0200

    correction mineur

--- linux_security/1x00_linux_fundamentals/8-log_policy.sh
+++ linux_security/1x00_linux_fundamentals/8-log_policy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mkdir -p "$1" && chown root:"$2" "$1" && chmod 2750 "$1" && echo -e "$1/*.log {\n    create 0640 root $2\n    missingok\n    rotate 7\n    compress\n    notifempty\n}" > /etc/logrotate.d/app
+mkdir -p "${1}" && chown root:${2} "${1}" && chmod 2750 "${1}" && echo -e "${1}/*.log {\n    create 0640 root ${2}\n    missingok\n    rotate 7\n    compress\n    notifempty\n}" > /etc/logrotate.d/app

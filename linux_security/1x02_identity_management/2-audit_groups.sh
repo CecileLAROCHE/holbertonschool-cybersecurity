ac57d8e31f8d73c33b9b88f632c44c35e35b3719
CecileLAROCHE <11458@holbertonstudents.com>
2026-04-22 15:13:09 +0200

    task 2

--- linux_security/1x02_identity_management/2-audit_groups.sh
+++ linux_security/1x02_identity_management/2-audit_groups.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+awk -F: '($3 >= 1000) {print $1}' "$1"  | while read user
+
+do 
+if id "$user" | grep -q disk
+then
+    echo "$user:disk"
+fi
+
+if id "$user" | grep -q docker
+then
+    echo "$user:docker"
+fi
+
+if id "$user" | grep -q shadow
+then
+    echo "$user:shadow"
+fi
+
+done

96880b849417ae7c623893e59e6e46322bc49abd
CecileLAROCHE <11458@holbertonstudents.com>
2026-05-12 16:59:03 +0200

    1 seule ligne

--- linux_security/1x03_system_visibility/8-who_listens.sh
+++ linux_security/1x03_system_visibility/8-who_listens.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-ss -lnt4p | awk -v port="$1" '
-NR > 1 {
-    split($4, a, ":")
-    if (a[2] == port) {
-        split($6, b, "\"")
-        print b[2]
-    }
-}'
+ss -lnt4p | awk -v port="$1" 'NR>1{split($4,a,":"); if(a[2]==port){split($6,b,"\""); print b[2]}}'
